f2d59c27b130eef30f851c615b07cb4412da388e
Wanyi Xie <xie00072@algonquinlive.com>
2021-10-04 21:33:09 -0400

    704binary search

---
+++ Binary Search/leetcode704/script.js
@@ -0,0 +1,19 @@
+let search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = math.floor((left + right)/2);
+        if (nums[mid] === target) {
+            return mid
+        }else if (nums[mid] < target) {
+            left = mid + 1
+        }else {
+            right = mid - 1
+        }
+    }
+
+    return -1
+}
+
+console.log(search)
